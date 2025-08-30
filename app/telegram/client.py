d26e3ee5eb81517ebdc4a654ea0705ab434074c9
shstprvvv <shestoperov01@gmail.com>
2025-08-30 14:01:31 +0300

    fix(telegram): Fix bot authentication in Docker

--- app/telegram/client.py
+++ app/telegram/client.py
@@ -1,6 +1,10 @@
 from telethon import TelegramClient
 
-def create_telegram_client(session_name: str, api_id: str, api_hash: str) -> TelegramClient:
+def create_telegram_client(session_name: str, api_id: str, api_hash: str, bot_token: str) -> TelegramClient:
+    """
+    Создает и настраивает клиент Telegram.
+    Для аутентификации бота, bot_token будет использован в методе .start().
+    """
     return TelegramClient(session_name, api_id, api_hash)
 
 
