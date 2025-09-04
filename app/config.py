3e062bf914c016ed590142b327cfec97a4c9038b
shstprvvv <shestoperov01@gmail.com>
2025-09-04 15:34:31 +0300

    Настройка: Увеличен интервал проверки вопросов WB до 10 минут

--- app/config.py
+++ app/config.py
@@ -13,7 +13,7 @@ def load_config():
         "OPENAI_API_BASE": os.getenv("OPENAI_API_BASE"),
         "WB_API_KEY": os.getenv("WB_API_KEY"),
         "WB_CHAT_POLLING_INTERVAL_SECONDS": int(os.getenv("WB_CHAT_POLLING_INTERVAL_SECONDS", 300)),
-        "WB_CHECK_INTERVAL_SECONDS": int(os.getenv("WB_CHECK_INTERVAL_SECONDS", 300)),
+        "WB_CHECK_INTERVAL_SECONDS": int(os.getenv("WB_CHECK_INTERVAL_SECONDS", 600)),
         "WB_CHAT_DEBUG": os.getenv("WB_CHAT_DEBUG", "false").lower() in ('true', '1', 't'),
         "TELEGRAM_MESSAGE_DELAY_SECONDS": int(os.getenv("TELEGRAM_MESSAGE_DELAY_SECONDS", 2)),
     }
