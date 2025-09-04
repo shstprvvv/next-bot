972268977de1f7b0175a6b78ee6449aa85a9a18f
shstprvvv <shestoperov01@gmail.com>
2025-09-04 19:07:26 +0300

    Фича: Логирование неотвеченных вопросов и расширение базы знаний

--- app/logging_config.py
+++ app/logging_config.py
@@ -2,7 +2,26 @@ import logging
 
 
 def setup_logging():
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    # Настройка основного логгера
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - [%(filename)s] %(message)s',
+        handlers=[
+            logging.StreamHandler()  # Вывод в консоль
+        ]
+    )
+
+    # Настройка логгера для неотвеченных вопросов
+    unanswered_logger = logging.getLogger('unanswered')
+    unanswered_logger.setLevel(logging.INFO)
+    unanswered_handler = logging.FileHandler('unanswered_questions.log', mode='a', encoding='utf-8')
+    unanswered_formatter = logging.Formatter('%(asctime)s - %(message)s')
+    unanswered_handler.setFormatter(unanswered_formatter)
+    unanswered_logger.addHandler(unanswered_handler)
+    unanswered_logger.propagate = False  # Не передавать сообщения в основной логгер
+
+    # Уменьшаем "шум" от сторонних библиотек
     logging.getLogger('httpx').setLevel(logging.WARNING)
+    logging.getLogger('telethon').setLevel(logging.WARNING)
 
 
